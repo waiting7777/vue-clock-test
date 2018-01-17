166d6392074c2ea17ace4e12098adba23e44b16a
waiting7777 <waiting0819@msn.com>
2018-01-17 14:54:00 +0800

    vue module browser and node bundle study

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const path = require('path')
+
+var config = {
+    output: {
+        path: path.resolve(__dirname + '/dist/')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: __dirname,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!less!css'
+            }
+        ]
+    },
+    externals: {
+        moment: 'moment'
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            sourceMap: false,
+            mangle: true,
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+}
+
+module.exports = [
+    merge(config, {
+        entry: path.resolve(__dirname + '/src/plugin.js'),
+        output: {
+            filename: 'vue-clock.min.js',
+            libraryTarget: 'window',
+            library: 'VueClock'
+        }
+    }),
+    merge(config, {
+        entry: path.resolve(__dirname + '/src/Clock.vue'),
+        output: {
+            filename: 'vue-clock.js',
+            libraryTarget: 'umd',
+            library: 'vue-clock',
+            umdNamedDefine: true
+        }
+    })
+]
